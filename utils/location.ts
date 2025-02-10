8f1332b57fc7e4c897ceda3aff7b332f4a165121
barakat <bamideleaduke@gmail.com>
2025-02-10 21:11:45 +0100

    remove keys

--- utils/location.ts
+++ utils/location.ts
@@ -1,6 +1,7 @@
 import Constants from "expo-constants";
+import { GOOGLE_API_KEY } from "@env";
 
-const GOOGLE_API_KEY = Constants.expoConfig?.extra?.GOOGLE_API_KEY;
+// const GOOGLE_API_KEY = Constants.expoConfig?.extra?.GOOGLE_API_KEY;
 
 export const getMapPreview = (lat: any, lng: any) => {
   const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
